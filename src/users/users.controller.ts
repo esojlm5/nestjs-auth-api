174b94f5a308ab2f047c579f9af7c8cebf71f3e7
esojlm5 <ne.ver1@outlook.com>
2025-05-21 16:43:33 -0500

    fix: update type getProfile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, UseGuards, Request, Get } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   @UseGuards(JwtAuthGuard)
   @Get('me')
-  getProfile(@Request() req) {
+  getProfile(@Request() req: { user: User }) {
     return req.user;
   }
 }
